058769cb31a598ed3472e46b314b8770ced003fe
Matt Watson <mwatson@renci.org>
2023-05-25 17:28:08 -0400

    bugfix: align test paths with previous refactor
    decruft

--- src/test/content.test.js
+++ src/test/content.test.js
@@ -3,29 +3,32 @@ const path = require('path');
 const YAML = require('yaml');
 const { hasDistinctElements } = require('./util')
 
-// determine which content to build the ui against.
+// determine which content directory to build the ui against.
 const shouldUseTestContent = process.env.TEST_CONTENT;
 const realContentPath = path.join('./src', 'content');
 const testContentPath = path.join('./src', 'test', 'content');
 const contentPath = shouldUseTestContent ? testContentPath : realContentPath;
 
-// paths with content
-const coursesFilePath = path.join(contentPath, 'courses.yaml');
-const instructorsFilePath = path.join(contentPath, 'instructors.yaml');
-const schedulesDirPath = path.join(contentPath, 'schedules');
+// paths with specific content
+const instructorsFilePath = path.join(testContentPath, 'instructors.yaml');
+const coursesDirPath = path.join(testContentPath, 'courses');
+const schedulesDirPath = path.join(testContentPath, 'schedules');
 
 // the content
-const coursesYaml = fs.readFileSync(coursesFilePath, 'utf8');
+const coursesYaml = fs.readdirSync(coursesDirPath)
+  .map(file => fs.readFileSync(path.join(coursesDirPath, file), 'utf8'));
 const instructorsYaml = fs.readFileSync(instructorsFilePath, 'utf8');
 const schedulesYaml = fs.readdirSync(schedulesDirPath)
-  .map(file => fs.readFileSync(path.join(schedulesDirPath, file), 'utf8'))
+  .map(file => fs.readFileSync(path.join(schedulesDirPath, file), 'utf8'));
 
 /*
  * tests
 **/
 
 test('course slugs should be unique', () => {
-  const courseSlugs = YAML.parse(coursesYaml).map(course => course.slug);
+  const courseSlugs = coursesYaml.map(courseYaml => {
+    return YAML.parse(courseYaml).slug;
+  }, [])
   expect(hasDistinctElements(courseSlugs)).toBe(true);
 });
 
